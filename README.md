294981b7c52310d2f5b69c0ec43270f7c69f91d2
Steven Lyles <steven@Stevens-Mac-Studio.local>
2022-12-29 16:30:41 -0800

    Refined nav arrow fixing bug not allowing multiple instances

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ A collection of JavaScript objects for easily creating web page interface widget
 This project is the results of another project (not yet in GitHub) that is an [LCARS](https://en.wikipedia.org/wiki/LCARS) based web interface. These widgets are done in the LCARS style and are meant to be components of that interface.
 
 This is work in progress. There is still much work to be done, but you can preview a live demo online of the progress so far at: [Web-Widgets](https://www.stevenlyles.net/playground/web_widgets/)
+
+## Demo vs Example
+In the source you will a demo folder and an example folder - a clarification is in order. Both of these folders contain html files showing how to use the various widgets. Examples are bare-bones usage demonstrating only what is need to create and use a widget. Demos go a step beyond and show the widget used in a real context with other UI elements.
+
+All demo and example pages will start with the prefixes "demo_" and "example_" respectively.
--- ww_js/web_widget_nav_arrows.js
+++ ww_js/web_widget_nav_arrows.js
@@ -1,141 +1,223 @@
 /*
- * Web Widgets - Indicator
- * Created by Steven M. Lyles
+ * ======================================================================================
+ * Web Widgets: Navigation Arrows
+ * Copyright © Steven M. Lyles
+ * https://github.com/steven-lyles/web-widgets
+ * https://www.stevenlyles.net
+ * ======================================================================================
  */
-'use strict';
 
-let callbackVert = null;
-let size_vert = 10;
-let color_vert = "blue";
-let hover_vert = "green";
-
-let callbackHorz = null;
-let size_horz = 10;
-let color_horz = "blue";
-let hover_horz = "green";
-
-//======================================================================================
-function gen_unique_id_from(str) {
-    var hash = Math.random() * (10 - 0) + 0;
-    var i = 0;
-    var char = "";
-
-    if (str.length !== 0) {
-        for (i = 0; i < str.length; i++) {
-            char = str.charCodeAt(i);
-            hash = ((hash << 5) - hash) + char;
-            hash = hash & hash;
-        }
-    }
-    return Math.abs(hash);
-}
+//====================================================================
+// This dictionary will be populated by the constructors and will keep
+// track of each instance's configuration for use by the callbacks
+let manifest = {};
 
 //=======================================================================================
-class NavArrow {
-    constructor(container_id, config) {
+// Collection of utility functions used by widget
+class Utils {
+
+    //===================================================================================
+    // Generates a random hash based on a given string. This is used to create unique ids
+    // to identify instances of the classes
+    static gen_unique_id_from(str) {
+        var hash = Math.random() * (10 - 0) + 0;
+        var i = 0;
+        var char = "";
+
+        if (str.length !== 0) {
+            for (i = 0; i < str.length; i++) {
+                char = str.charCodeAt(i);
+                hash = ((hash << 5) - hash) + char;
+                hash = hash & hash;
+            }
+        }
+        return Math.abs(hash);
+    }
+} // class Utils
+
+//=======================================================================================
+// Base class for nav arrow classes
+class NavArrows {
+    constructor(container_id, config, callback) {
         this.config = config;
         this.id = container_id;
-        this.widget_id = gen_unique_id_from(container_id);
+        this.widget_id = Utils.gen_unique_id_from(container_id);
+        manifest[this.widget_id] = config;
+        manifest[this.widget_id]["callback"] = callback;
     }
-}
+
+    //===================================================================================
+    get_id() { return this.widget_id; }
+} /* class NavArrow */
 
 //=======================================================================================
-class NavArrowsVertical extends NavArrow {
+// Represents a vertical nav arrow group
+// @param [String] container_id - id attribute of html parent container
+// @param [JSON] config - configuration of this arrow group as follows:
+//   let config_vert = {
+//         "aspect"  : 3,         // how much the arrow length is greater than width
+//         "rounded" : false,     // true - base of arrow will be rounded; false - straight
+//         "size"    : 20,        // the smallest dimension (px); the longest will be "aspect" x "size"
+//         "gap"     : 10,        // gap between arrows and target (px)
+//         "color"   : "#32557f", // cadet blue; color of arrow (background is same as container)
+//         "hover"   : "#99dd66"  // color on hover
+//     }
+// @param [Object] callback - function called upon nav arrow click
+class NavArrowsVertical extends NavArrows {
+    //===================================================================================
     constructor(container_id, config, callback) {
-        super(container_id, config);
+        super(container_id, config, callback);
         this.build_widget();
-        callbackVert = callback;
-        size_vert = config.size;
-        color_vert = config.color;
-        hover_vert = config.hover;
+        this.set_container_css();
+        this.set_css_vert();
     }
 
+    //===================================================================================
+    // Set the css styling for the parent container (id from construction)
+    set_container_css() {
+        $(`#${this.id}`).css("display", "flex");
+        $(`#${this.id}`).css("flex-direction", "column");
+        $(`#${this.id}`).css("align-items", "center");
+        $(`#${this.id}`).css("justify-content", "center");
+        $(`#${this.id}`).css("gap", `${this.config.gap}px`);
+    }
+
+    //===================================================================================
+    // Set the css styling as specified by the passed config
     set_css_vert() {
         $(".nav-arrow-up").css("border-left", `${this.config.size}px solid transparent`);
         $(".nav-arrow-up").css("border-right", `${this.config.size}px solid transparent`);
-        $(".nav-arrow-up").css("border-bottom", `${this.config.size*3}px solid ${this.config.color}`);
+        $(".nav-arrow-up").css("border-bottom", `${this.config.size*this.config.aspect}px solid ${this.config.color}`);
         if (this.config.rounded) { $(".nav-arrow-up").css("border-radius", "50%"); }
 
         $(".nav-arrow-down").css("border-left", `${this.config.size}px solid transparent`);
         $(".nav-arrow-down").css("border-right",`${this.config.size}px solid transparent`);
-        $(".nav-arrow-down").css("border-top", `${this.config.size*3}px solid ${this.config.color}`); // cadet blue: #32557f
+        $(".nav-arrow-down").css("border-top", `${this.config.size*this.config.aspect}px solid ${this.config.color}`); // cadet blue: #32557f
         if (this.config.rounded) { $(".nav-arrow-down").css("border-radius", "50%"); }
     }
 
+    //===================================================================================
+    // Insert the up and down arrow tags in the parent html container as 1st and last
+    // elements. Be sure to add the target tags before constructing the arrow group.
     build_widget() {
-        $("#" + this.id).prepend("<div id='up-" + this.widget_id + "'class='nav-arrow-up'></div>");
-        $("#" + this.id).append("<div id='down-" + this.widget_id + "'class='nav-arrow-down'></div>");
-        this.set_css_vert();
+        $("#" + this.id).prepend("<div id='up-" + this.widget_id + "'class='nav-arrow nav-arrow-up'></div>");
+        $("#" + this.id).append("<div id='down-" + this.widget_id + "'class='nav-arrow nav-arrow-down'></div>");
     }
-}
+} /* class NavArrowsVertical */
 
 //=======================================================================================
-class NavArrowsHorizontal extends NavArrow {
+// Represents a horizontal nav arrow group
+// @param [String] container_id - id attribute of html parent container
+// @param [JSON] config - configuration of this arrow group as follows:
+//   let config_horz = {
+//         "aspect"  : 3,         // how much the arrow length is greater than width
+//         "rounded" : false,     // true - base of arrow will be rounded; false - straight
+//         "size"    : 20,        // the smallest dimension (px); the longest will be "aspect" x "size"
+//         "gap"     : 10,        // gap between arrows and target (px)
+//         "color"   : "#32557f", // cadet blue; color of arrow (background is same as container)
+//         "hover"   : "#99dd66"  // color on hover
+//     }
+// @param [Object] callback - function called upon nav arrow click
+class NavArrowsHorizontal extends NavArrows {
     constructor(container_id, config, callback) {
-        super(container_id, config);
+        super(container_id, config, callback);
         this.build_widget();
-        callbackHorz = callback;
-        size_horz = config.size;
-        color_horz = config.color;
-        hover_horz = config.hover;
+        // manifest[this.widget_id] = config;
+        this.set_container_css();
+        this.set_css_horz();
     }
 
+    //===================================================================================
+    // Set the css styling for the parent container (id from construction)
+    set_container_css() {
+        $(`#${this.id}`).css("display", "flex");
+        $(`#${this.id}`).css("flex-direction", "row");
+        $(`#${this.id}`).css("align-items", "center");
+        $(`#${this.id}`).css("justify-content", "center");
+        $(`#${this.id}`).css("gap", `${this.config.gap}px`);
+    }
+
+    //===================================================================================
+    // Set the css styling as specified by the passed config
     set_css_horz() {
         $(".nav-arrow-left").css("border-top", `${this.config.size}px solid transparent`);
         $(".nav-arrow-left").css("border-bottom", `${this.config.size}px solid transparent`);
-        $(".nav-arrow-left").css("border-right", `${this.config.size*3}px solid ${this.config.color}`);
+        $(".nav-arrow-left").css("border-right", `${this.config.size*this.config.aspect}px solid ${this.config.color}`);
+        $(".nav-arrow-left").css("margin-right", `${this.config.gap}px`);
         if (this.config.rounded) { $(".nav-arrow-left").css("border-radius", "50%"); }
 
         $(".nav-arrow-right").css("border-top", `${this.config.size}px solid transparent`);
         $(".nav-arrow-right").css("border-bottom",`${this.config.size}px solid transparent`);
-        $(".nav-arrow-right").css("border-left", `${this.config.size*3}px solid ${this.config.color}`); // cadet blue: #32557f
+        $(".nav-arrow-right").css("border-left", `${this.config.size*this.config.aspect}px solid ${this.config.color}`); // cadet blue: #32557f
+        $(".nav-arrow-right").css("margin-left", `${this.config.gap}px`);
         if (this.config.rounded) { $(".nav-arrow-right").css("border-radius", "50%"); }
     }
 
+    //===================================================================================
+    // Insert the left and right arrow tags in the parent html container as 1st and last
+    // elements. Be sure to add the target tags before constructing the arrow group.
     build_widget() {
-        $("#" + this.id).prepend("<div id='left-" + this.widget_id + "'class='nav-arrow-left'></div>");
-        $("#" + this.id).append("<div id='right-" + this.widget_id + "'class='nav-arrow-right'></div>");
-        this.set_css_horz();
+        $("#" + this.id).prepend("<div id='left-" + this.widget_id + "'class='nav-arrow nav-arrow-left'></div>");
+        $("#" + this.id).append("<div id='right-" + this.widget_id + "'class='nav-arrow nav-arrow-right'></div>");
     }
-}
+} /* class NavArrowsHorizontal */
 
 //=======================================================================================
 $(document).ready(function() {
 
-    $(".nav-arrow-up").click( function() { callbackVert("up") });
-    $(".nav-arrow-down").click( function() { callbackVert("down") });
-    $(".nav-arrow-left").click( function() { callbackHorz("left") });
-    $(".nav-arrow-right").click( function() { callbackHorz("right") });
+    //-------------------------------------------------------------------------------------
+    // Handle arrow click; passes which arrow ("up", "down", "left", "right") & instance id
+    $(".nav-arrow").click( function() {
+        let id = `${this.id}`.split("-")[1];
+        let direction = `${this.id}`.split("-")[0];
+        manifest[id].callback.apply(this, [direction, id]);
+    });
 
+    //----------------------
+    // Handle hover up arrow
     $(".nav-arrow-up").hover(function(){
-        $(this).css("border-bottom", `${size_vert*3}px solid ${hover_vert}`);
-        $(this).css("cursor", "pointer");
+        let id = `${this.id}`.split("-")[1];
+        $(`#up-${id}`).css("border-bottom", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].hover}`);
+        $(`#up-${id}`).css("cursor", "pointer");
     }, function(){
-        $(this).css("border-bottom", `${size_vert*3}px solid ${color_vert}`);
-        $(this).css("cursor", "default");
+        let id = `${this.id}`.split("-")[1];
+        $(`#up-${id}`).css("border-bottom", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].color}`);
+        $(`#up-${id}`).css("cursor", "default");
     });
 
+    //------------------------
+    // Handle hover down arrow
     $(".nav-arrow-down").hover(function(){
-        $(this).css("border-top", `${size_vert*3}px solid ${hover_vert}`);
-        $(this).css("cursor", "pointer");
+        let id = `${this.id}`.split("-")[1];
+        $(`#down-${id}`).css("border-top", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].hover}`);
+        $(`#down-${id}`).css("cursor", "pointer");
     }, function(){
-        $(this).css("border-top", `${size_vert*3}px solid ${color_vert}`);
-        $(this).css("cursor", "default");
+        let id = `${this.id}`.split("-")[1];
+        $(`#down-${id}`).css("border-top", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].color}`);
+        $(`#down-${id}`).css("cursor", "default");
     });
 
+    //------------------------
+    // Handle hover left arrow
     $(".nav-arrow-left").hover(function(){
-        $(this).css("border-right", `${size_horz*3}px solid ${hover_horz}`);
-        $(this).css("cursor", "pointer");
+        let id = `${this.id}`.split("-")[1];
+        $(`#left-${id}`).css("border-right", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].hover}`);
+        $(`#left-${id}`).css("cursor", "pointer");
     }, function(){
-        $(this).css("border-right", `${size_horz*3}px solid ${color_horz}`);
-        $(this).css("cursor", "default");
+        let id = `${this.id}`.split("-")[1];
+        $(`#left-${id}`).css("border-right", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].color}`);
+        $(`#left-${id}`).css("cursor", "default");
     });
 
+    //-------------------------
+    // Handle hover right arrow
     $(".nav-arrow-right").hover(function(){
-        $(this).css("border-left", `${size_horz*3}px solid ${hover_horz}`);
-        $(this).css("cursor", "pointer");
+        let id = `${this.id}`.split("-")[1];
+        $(`#right-${id}`).css("border-left", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].hover}`);
+        $(`#right-${id}`).css("cursor", "pointer");
     }, function(){
-        $(this).css("border-left", `${size_horz*3}px solid ${color_horz}`);
-        $(this).css("cursor", "default");
+        let id = `${this.id}`.split("-")[1];
+        $(`#right-${id}`).css("border-left", `${manifest[id].size*manifest[id].aspect}px solid ${manifest[id].color}`);
+        $(`#right-${id}`).css("cursor", "default");
     });
 });
