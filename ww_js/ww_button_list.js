1a5d45f7bce9e79d05f17b1fe2da1824f17f8953
Steven Lyles <steven.lyles@gmail.com>
2023-01-27 21:57:20 -0800

    Got button list accordion straigtend out with prototype and usage examples

--- ww_js/ww_button_list.js
+++ ww_js/ww_button_list.js
@@ -32,17 +32,14 @@ class WebWidgetMenuList {
     gen_widget(container_id, config, widget_id) {
         $.each(config["items"], function( index, value ) {
             if (config["accordion"]) {
-                $(`#${container_id}`).append(`<div id='selection-${index}-${widget_id}' class='choice'></div>`);
-                $(`#selection-${index}-${widget_id}`).append(`<div id='selection-${index}-button-${widget_id}' class='button'>${value["name"]}</div>`);
-                $(`#selection-${index}-${widget_id}`).append(`<div id='selection-${index}-desc-${widget_id}', class='desc'>${value["desc"]}</div>`);
-                $(`#${container_id}`).append(`<div id='selection-content-container-${index}-${widget_id}' class='selection-content-container'></div>`);
-                let embedded_page = "";
-                embedded_page += `<embed id='selection-page-${index}-${widget_id}`;
-                embedded_page += "' type='text/html' ";
-                embedded_page += "src='" + value["link"] + "' ";
-                embedded_page += "width='" + value["width"] + "' height='" + value["height"] + "'>";
-                // console.log(embedded_page);
-                $(`#selection-content-container-${index}-${widget_id}`).append(embedded_page);
+                $(`#${container_id}`).append(`<div id='selection-${index}-${widget_id}' class='choice-${widget_id} choice'></div>`);
+                $(`#selection-${index}-${widget_id}`).append(`<div id='selection-${index}-button-${widget_id}' class='button-${widget_id}'>${value["name"]}</div>`);
+                $(`#selection-${index}-${widget_id}`).append(`<div id='selection-${index}-desc-${widget_id}', class='desc-${widget_id}'>${value["desc"]}</div>`);
+                $(`#${container_id}`).append(`<div id='content-${index}-container-${widget_id}' class='hidden-${widget_id}'></div>`);
+                $(`#content-${index}-container-${widget_id}`).append(`<div id='content-${index}-${widget_id}' class='content-${widget_id}'></div>`);
+
+                let target_page = `<iframe id='content'-${index}-src-${widget_id} src='${value.link}' class='page-content-${widget_id}'></iframe>`;
+                $(`#content-${index}-${widget_id}`).append(target_page);
 
             } else {
                 $(`#${container_id}`).append(`<a id='link-${index}-${widget_id}' href='${value["link"]}' target='_blank'></a>`);
@@ -62,55 +59,74 @@ class WebWidgetMenuList {
         $(`#${this.id}`).css("background-color", config.color_map[ config["background_color"] ].hex);
         $(`#${this.id}`).css("display", "flex");
         $(`#${this.id}`).css("flex-direction", "column");
-        $(`#${this.id}`).css("gap", "10px");
+        $(`#${this.id}`).css("gap", config.gap);
         $(`#${this.id}`).css("height", "auto");
-        $(`#${this.id}`).css("width", "100%");
-        $(`#${this.id}`).css("padding-top", "10px");
+        $(`#${this.id}`).css("width", "99%");
+        $(`#${this.id}`).css("padding", "10px");
 
-        $(".choice").css("display", "flex");
-        $(".choice").css("flex-direction", "row");
-        $(".choice").css("background", config.color_map[ config["background_color"] ].hex);
-        $(".choice").css("width", config["width"]);
+        $(`.choice-${this.widget_id}`).css("display", "flex");
+        $(`.choice-${this.widget_id}`).css("flex-direction", "row");
+        $(`.choice-${this.widget_id}`).css("background", config.color_map[ config["background_color"] ].hex);
+        $(`.choice-${this.widget_id}`).css("width", config["width"]);
         let h = parseInt(config["height"].replace('px', '')) + 4; // adjust height for borders
-        $(".choice").css("height", h.toString() + "px");
-        $(".choice").css("float", "left");
-        $(".choice:hover").css("cursor", "pointer");
+        $(`.choice-${this.widget_id}`).css("height", h.toString() + "px");
+        $(`.choice-${this.widget_id}`).css("float", "left");
+        $(`.choice-${this.widget_id}`).css("cursor", "pointer");
+        $(`.choice-${this.widget_id}`).css("margin-right", "20px");
 
-        $(".button").css("background", config.color_map[ config["button"]["background_color"] ].hex);
-        $(".button").css("color", config.color_map[ config["button"]["font_color"] ].hex);
-        $(".button").css("width", config["button"]["width"]);
-        $(".button").css("height", config["height"]);
-        $(".button").css("font-size", config["button"]["font_size"]);
-        $(".button").css("display", "flex");
-        $(".button").css("align-items", "center");
-        $(".button").css("justify-content", "center");
-        $(".button").css("border-top", "2px solid " + config["button"]["background_color"]);
-        $(".button").css("border-bottom", "2px solid " + config["button"]["background_color"]);
-        $(".button").css("border-left", "2px solid " + config["button"]["background_color"]);
-        $(".button").css("border-radius", config["button"]["border_radius"] + " 0px 0px " + config["button"]["border_radius"]);
+        $(`.button-${this.widget_id}`).css("background", config.color_map[ config["button"]["background_color"] ].hex);
+        $(`.button-${this.widget_id}`).css("color", config.color_map[ config["button"]["font_color"] ].hex);
+        $(`.button-${this.widget_id}`).css("width", config["button"]["width"]);
+        $(`.button-${this.widget_id}`).css("height", config["height"]);
+        $(`.button-${this.widget_id}`).css("font-size", config["button"]["font_size"]);
+        $(`.button-${this.widget_id}`).css("display", "flex");
+        $(`.button-${this.widget_id}`).css("align-items", "center");
+        $(`.button-${this.widget_id}`).css("justify-content", "center");
+        $(`.button-${this.widget_id}`).css("border-top", "2px solid " + config["button"]["background_color"]);
+        $(`.button-${this.widget_id}`).css("border-bottom", "2px solid " + config["button"]["background_color"]);
+        $(`.button-${this.widget_id}`).css("border-left", "2px solid " + config["button"]["background_color"]);
+        $(`.button-${this.widget_id}`).css("border-radius", config["button"]["border_radius"] + " 0px 0px " + config["button"]["border_radius"]);
 
-        $(".desc").css("background-color", config.color_map[ config["description"]["background_color"] ].hex);
-        $(".desc").css("color", config.color_map[ config["description"]["font_color"] ].hex);
-        $(".desc").css("width", config["description"]["width"]);
-        $(".desc").css("height", (h-8).toString() + "px");
-        $(".desc").css("font-size", config["description"]["font_size"]);
-        $(".desc").css("display", "flex");
-        $(".desc").css("align-items", "center");
-        $(".desc").css("justify-content", "flex-start");
-        $(".desc").css("border-top", "2px solid " + config.color_map[ config["description"]["outline_color"] ].hex);
-        $(".desc").css("border-bottom", "2px solid " + config.color_map[ config["description"]["outline_color"] ].hex);
-        $(".desc").css("border-right", "2px solid " + config.color_map[ config["description"]["outline_color"] ].hex);
-        $(".desc").css("border-radius", "0px " + config["description"]["border_radius"] + " " + config["description"]["border_radius"] + " 0px");
-        $(".desc").css("padding-left", "20px");
+        $(`.desc-${this.widget_id}`).css("background-color", config.color_map[ config["description"]["background_color"] ].hex);
+        $(`.desc-${this.widget_id}`).css("color", config.color_map[ config["description"]["font_color"] ].hex);
+        $(`.desc-${this.widget_id}`).css("width", config["description"]["width"]);
+        $(`.desc-${this.widget_id}`).css("height", (h-8).toString() + "px");
+        $(`.desc-${this.widget_id}`).css("font-size", config["description"]["font_size"]);
+        $(`.desc-${this.widget_id}`).css("display", "flex");
+        $(`.desc-${this.widget_id}`).css("align-items", "center");
+        $(`.desc-${this.widget_id}`).css("justify-content", "flex-start");
+        $(`.desc-${this.widget_id}`).css("border-top", "2px solid " + config.color_map[ config["description"]["outline_color"] ].hex);
+        $(`.desc-${this.widget_id}`).css("border-bottom", "2px solid " + config.color_map[ config["description"]["outline_color"] ].hex);
+        $(`.desc-${this.widget_id}`).css("border-right", "2px solid " + config.color_map[ config["description"]["outline_color"] ].hex);
+        $(`.desc-${this.widget_id}`).css("border-radius", "0px " + config["description"]["border_radius"] + " " + config["description"]["border_radius"] + " 0px");
+        $(`.desc-${this.widget_id}`).css("padding-left", "20px");
+        $(`.desc-${this.widget_id}`).css("text-decoration", "none");
 
         if (config["accordion"]) {
-            $(".selection-content-container").css("background-color", config.color_map[ config["accordion-content"]["background_color"] ].hex);
-            $(".selection-content-container").css("margin-left", config["accordion-content"]["margin_left"]);
-            $(".selection-content-container").css("display", "none");
-            let adjust = parseInt(config["accordion-content"]["margin_left"].replace('px', '')) * 2;
-            let width = parseInt(config["width"].replace('px', "")) - adjust; // adjust for margins
-            $(".selection-content-container").css("width", width + "px");
-            $(".selection-content-container").css("height", "auto");
+            $(`.content-${this.widget_id}`).css("display", "flex");
+            $(`.content-${this.widget_id}`).css("flex-direction", "column");
+            $(`.content-${this.widget_id}`).css("justify-content", "center");
+            $(`.content-${this.widget_id}`).css("background-color", "white");
+            $(`.content-${this.widget_id}`).css("width", "100%");
+            $(`.content-${this.widget_id}`).css("height", "100%");
+            $(`.content-${this.widget_id}`).css("float", "left");
+            $(`.content-${this.widget_id}`).css("cursor", "pointer");
+
+            $(`.page-content-${this.widget_id}`).css("overflow-y", "scroll");
+            $(`.page-content-${this.widget_id}`).css("border", "none");
+            $(`.page-content-${this.widget_id}`).css("height", "100%");
+            $(`.page-content-${this.widget_id}`).css("margin-bottom", "15px");
+
+            $(`.hidden-${this.widget_id}`).css("display", "none");
+            $(`.hidden-${this.widget_id}`).css("margin-left", "10px");
+            $(`.hidden-${this.widget_id}`).css("margin-right", "10px");
+            $(`.hidden-${this.widget_id}`).css("background-color", "pink");
+
+            let id = this.widget_id;
+            $.each(config["items"], function( index, value ) {
+                console.log(`#content-${index}-container-${id}`);
+                $(`#content-${index}-container-${id}`).css("height", `${value.size + 30}px`);
+            });
         }
     }
 
@@ -138,16 +154,17 @@ $(document).ready(function() {
 
     //--------------------------------------------------------------------------------
     $(document).on('click', '.choice', function(){
+
         let index = (this.id).split("-")[1];
         let id = this.id.split("-")[2]
-        let count = $('.selection-content-container').length;
+        let count = $(`.choice-${id}`).length;
 
         for (let i = 0; i < count; i++) {
             if (i == index) {
-                $(`#selection-content-container-${i}-${id}`).slideToggle("slow");
+                $(`#content-${i}-container-${id}`).slideToggle("slow");
             }
             else {
-                $(`#selection-content-container-${i}-${id}`).slideUp("medium");
+                $(`#content-${i}-container-${id}`).slideUp("medium");
             }
         }
     });
