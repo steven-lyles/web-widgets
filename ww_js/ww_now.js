72d6e4593214d0d88b6d0d9a25adbeee3c945f3b
Steven Lyles <steven.lyles@gmail.com>
2023-02-18 19:19:17 -0800

    Removed background color from now widget so be transparent

--- ww_js/ww_now.js
+++ ww_js/ww_now.js
@@ -22,7 +22,6 @@ class Now {
     //===================================================================================
     gen_css() {
         $("#time-panel").css("font", `400 ${this.config.font_size}px/1.5 "Roboto", sans-serif`);
-        $("#time-panel").css("background-color", this.config.background_color);
         $("#time-panel").css("color", this.config.font_color);
         $("#time-panel").css("padding", "10px 0px 5px 0px");
         $("#time-panel").css("width", this.config.width);
